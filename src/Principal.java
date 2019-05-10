7d23b4e17283de076204db411e1c25da17a3dd45
Luis Miguel <trabajodeluismi@gmail.com>
2019-05-10 10:50:23 +0200

    sexto commit

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Línea 1 modificar");
+		System.out.println("Línea 1 añadimos de nuevo");
 		//System.out.println("Línea 2");
 		System.out.println("Línea 3");
 		System.out.println("Línea 4");
